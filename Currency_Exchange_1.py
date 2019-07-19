860a823347e090c2ce602ea3716b7c9151755648
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-19 09:27:54 +0530

    Add files via upload

---
+++ Currency_Exchange_1.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 18 06:29:28 2019
+
+@author: khushal
+"""
+
+### https://forex-python.readthedocs.io/en/latest/usage.html
+
+from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+import datetime
+import pandas as pd
+import numpy as np
+
+Start_date = datetime.datetime.now() + datetime.timedelta(-120)
+Current_date = datetime.datetime.now()
+print(Start_date,Current_date)
+
+date_value_df = pd.date_range(start=Start_date, end=Current_date)
+#print(date_value_df)
+
+currency_df = pd.DataFrame(columns=('Date','USD_to_INR_Rate','GBP_to_INR_Rate'))
+                                    #,'INR_symbol'))
+USD_to_INR_Rate = []
+GBP_to_INR_Rate = []
+
+#print(currency_df)
+for i in range(len(date_value_df)):
+    c = CurrencyRates()
+    #print(date_value_df[i])
+    USD_to_INR_Rate = round(c.get_rates('USD',date_value_df[i]).get('INR'),2)
+    GBP_to_INR_Rate = round(c.get_rates('GBP',date_value_df[i]).get('INR'),2)
+    dict_rates = {'Date':date_value_df[i],'USD_to_INR_Rate':USD_to_INR_Rate,'GBP_to_INR_Rate':GBP_to_INR_Rate}
+    currency_df = currency_df.append(dict_rates,ignore_index=True)
+    #print("USD_to_INR_Rate = ",USD_to_INR_Rate)
+    #print("GBP_to_INR_Rate = ", GBP_to_INR_Rate)
+
+c = CurrencyCodes()
+#currency_df['INR_symbol'] =  c.get_symbol('INR')
+currency_df['Date'] = currency_df['Date'].dt.strftime('%m/%d/%Y')
+currency_df['Date'] = pd.to_datetime(currency_df.Date,format='%m/%d/%Y')
+#print("symbol = ",currency_df['INR_symbol'])
+currency_df.index= currency_df['Date']
+# currency_df.drop(['Date'], axis=1,inplace=True) # activate this line to do Graph plotting
+print(currency_df)
+
+'''
+import matplotlib.pyplot as plt
+
+#%matplotlib inline
+
+from matplotlib.pylab import rcParams
+rcParams['figure.figsize'] = 20,10
+plt.figure(figsize=(16,8))
+plt.plot(currency_df,label='Currency Exchange w.r.t Time')
+'''
+##################
+
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,LSTM
+
+data = currency_df.sort_index(ascending=True,axis=0)
+new_data = pd.DataFrame(index=range(0,len(currency_df)),columns=['Date','USD_to_INR_Rate'])
+
+for i in range(len(currency_df)):
+    new_data['Date'][i] = currency_df['Date'][i]
+    new_data['USD_to_INR_Rate'][i] = currency_df['USD_to_INR_Rate'][i]
+    #new_data['GBP_to_INR_Rate'][i] = currency_df['GBP_to_INR_Rate'][i]
+
+print("new_data shape= ",new_data.shape)
+
+new_data.index = new_data.Date
+new_data.drop(['Date'], axis=1,inplace=True) 
+
+dataset = new_data.values
+
+train = dataset[0:84,:]
+valid = dataset[84:,:]
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+scaled_data = scaler.fit_transform(dataset)
+
+x_train, y_train = [],[]
+
+for i in range(6,len(train)):
+    x_train.append(scaled_data[i-6:i,0])
+    y_train.append(scaled_data[i,0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train= np.reshape(x_train,(x_train.shape[0],x_train.shape[1],1))
+
+print("len(train)",len(train))
+print("x_train, y_train ",len(x_train), len(y_train ))
+
+model= Sequential()
+model.add(LSTM(units=50,return_sequences=True,input_shape=(np.array(x_train).shape[1],1)))
+model.add(LSTM(units=50))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error',optimizer='adam')
+model.fit(x_train,y_train,epochs=4,batch_size=2,verbose=3)
+
+inputs = new_data[len(new_data)-len(valid)-6:].values
+inputs= inputs.reshape(-1,1)
+print("inputs = ",len(inputs))
+inputs= scaler.transform(inputs)
+
+x_test = []
+
+for i in range(6,inputs.shape[0]):
+    x_test.append(inputs[i-6:i,0])
+
+x_test = np.array(x_test)
+
+print("x_test len =",len(x_test ))
+x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+
+price = model.predict(x_test)
+price = scaler.inverse_transform(price)
+
+rms = np.sqrt(np.mean(np.power((valid-price),2)))
+
+print("RMS =",rms)
+print("price = ",price)
