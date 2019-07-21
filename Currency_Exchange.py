e67c44e2a5d43db712c942e9e28b4f9a487b629f
Khushal Sawant <47523964+Khushalsawant@users.noreply.github.com>
2019-07-21 13:32:50 +0530

    Add files via upload

--- Currency_Exchange.py
+++ Currency_Exchange.py
@@ -17,10 +17,10 @@ import pandas as pd
 import numpy as np
 
 #Start_date = datetime.datetime.now() + datetime.timedelta(-120)
-Start_date = datetime.datetime.now() + datetime.timedelta(-1220)
+Start_date = datetime.datetime.now() + datetime.timedelta(-602)
 Current_date = datetime.datetime.now() + datetime.timedelta(-2)
 #Current_date = datetime.datetime.now()
-print(Start_date,Current_date)
+print("Start_date = ",Start_date,"\n Current_date = ",Current_date)
 
 date_value_df = pd.date_range(start=Start_date, end=Current_date)
 #print(date_value_df)
@@ -56,7 +56,7 @@ import matplotlib.pyplot as plt
 #%matplotlib inline
 
 from matplotlib.pylab import rcParams
-rcParams['figure.figsize'] = 20,10
+rcParams['figure.figsize'] = 20,15
 plt.figure(figsize=(16,8))
 plt.plot(currency_df,label='Currency Exchange w.r.t Time')
 '''
@@ -64,7 +64,7 @@ plt.plot(currency_df,label='Currency Exchange w.r.t Time')
 
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense,Dropout,LSTM
+from keras.layers import Dense,Dropout,LSTM,Masking
 
 data = currency_df.sort_index(ascending=True,axis=0)
 new_data = pd.DataFrame(index=range(0,len(currency_df)),columns=['Date','USD_to_INR_Rate'])
@@ -81,8 +81,8 @@ new_data.drop(['Date'], axis=1,inplace=True)
 
 dataset = new_data.values
 
-train = dataset[0:980,:]
-valid = dataset[980:,:]
+train = dataset[0:498,:]
+valid = dataset[498:,:]
 
 scaler = MinMaxScaler(feature_range=(0, 1))
 
@@ -90,8 +90,8 @@ scaled_data = scaler.fit_transform(dataset)
 
 x_train, y_train = [],[]
 
-for i in range(60,len(train)):
-    x_train.append(scaled_data[i-60:i,0])
+for i in range(15,len(train)):
+    x_train.append(scaled_data[i-15:i,0])
     y_train.append(scaled_data[i,0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
@@ -101,8 +101,14 @@ print("len(train)",len(train))
 print("x_train, y_train ",len(x_train), len(y_train ))
 
 model= Sequential()
-model.add(LSTM(units=50,return_sequences=True,activation='relu',
+
+# Masking layer for pre-trained embeddings
+model.add(Masking(mask_value=0.0))
+
+# Recurrent layer
+model.add(LSTM(units=5,return_sequences=True,activation='relu',
                input_shape=(np.array(x_train).shape[1],1)))
+'''
 model.add(Dropout(0.2))
 model.add(LSTM(units=50,return_sequences=True))
 model.add(Dropout(0.2))
@@ -110,37 +116,40 @@ model.add(LSTM(units = 50, return_sequences = True))
 model.add(Dropout(0.2))
 model.add(LSTM(units = 50, return_sequences = True))
 model.add(Dropout(0.2))
+'''
 model.add(LSTM(units = 50))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error',optimizer='adam')
-model.fit(x_train,y_train,epochs=50,batch_size=32,verbose=0)
+model.fit(x_train,y_train,epochs=25,batch_size=80,verbose=2)
+#model.fit(x_train,y_train,epochs=335,batch_size=80,verbose=2) ##Very Nearer Prediction Values
 
-inputs = new_data[len(new_data)-len(valid)-60:].values
+inputs = new_data[len(new_data)-len(valid)-15:].values
 inputs= inputs.reshape(-1,1)
-print("inputs = ",len(inputs))
+print("inputs = ",len(inputs)) 
 inputs= scaler.transform(inputs)
-
+ 
 x_test = []
 
-for i in range(60,inputs.shape[0]):
-    x_test.append(inputs[i-60:i,0])
+for i in range(15,inputs.shape[0]):
+    x_test.append(inputs[i-15:i,0])
 
 x_test = np.array(x_test)
 
-print("x_test len =",len(x_test ))
+print("x_test len =",len(x_test))
 x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
 
-price = model.predict(x_test,verbose=0)
+price = model.predict(x_test)#,verbose=1)
 price = scaler.inverse_transform(price)
 
 rms = np.sqrt(np.mean(np.power((valid-price),2)))
 
-print("RMS =",rms)
+print('Train Score: %.2f RMSE' % (rms))
+
 
 print("Len of price",len(price))
 
-for i in range(len(price)):
-    print("price = ",price[i],"Value of i",i)
+for i in range(5):
+    print("price = ",np.round(price[i],2)," Future Date of ", (Current_date + datetime.timedelta(i+1)))
 
 # into hours, minutes and seconds 
 import datetime 
